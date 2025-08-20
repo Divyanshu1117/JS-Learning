534abed838cbc57869dff064fce7959a28e368fb
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-08-20 18:01:34 +0530

    Add new files

--- 102_pdfTools/server.js
+++ 102_pdfTools/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const multer = require('multer')
-const {mergePdfs} = require('./merge')
+const { mergePdfs } = require('./merge')
 
-const upload = multer({ dest: 'uploads/'})
+const upload = multer({ dest: 'uploads/' })
 app.use('/static', express.static('public'))
 const port = 3000
 
@@ -12,13 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 })
 
-app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
   console.log(req.files);
   let d = await mergePdfs(
-  path.join(__dirname, req.files[0].path), 
-  path.join(__dirname, req.files[1].path));
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path));
   res.redirect(`http://localhost:3000/static/${d}.pdf`)
-  
+
   //res.send({data: req.files})
 })
 
@@ -26,11 +26,11 @@ app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
 
-// npm init -y   
-// npm i express   
-// npm i -g nodemon 
-// npm fund 
-// nodemon -v       
+// npm init -y
+// npm i express
+// npm i -g nodemon
+// npm fund
+// nodemon -v
 // nodemon server.js
 // npm i multer
 // 4.1.2
--- 102_pdfTools/tempCodeRunnerFile.js
+++ 102_pdfTools/tempCodeRunnerFile.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const multer = require('multer')
-const {mergePdfs} = require('./merge')
+const { mergePdfs } = require('./merge')
 
-const upload = multer({ dest: 'uploads/'})
+const upload = multer({ dest: 'uploads/' })
 app.use('/static', express.static('public'))
 const port = 3000
 
@@ -12,11 +12,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 })
 
-app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
   console.log(req.files);
   await mergePdfs(
-  path.join(__dirname, req.files[0].path), 
-  path.join(__dirname, req.files[1].path));
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path));
   res.redirect("http://localhost:3000/static/merged.pdf")
   //res.send({data: req.files})
 })
